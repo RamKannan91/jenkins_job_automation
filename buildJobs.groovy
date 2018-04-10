05da138d0f4712e0d23c37194b382cb1a4aefc8c
usertest <ram_kannan@symantec.com>
2018-04-10 18:04:28 +0530

    updating files

--- buildJobs.groovy
+++ buildJobs.groovy
@@ -3,15 +3,17 @@ import common.Common
 def pipelinScript = "jobs/Build_Applications"
 
 def disableProj = ["AccountManagementPortal": true,
-                   "BillingCenter"     : true
+                   "BillingCenter": true,
+                   "ClaimCenter": true,
+                   "ContactManager": true
                    ]
 
 Common.list_projName.eachWithIndex { projName, idx ->
     Common.list_Version.eachWithIndex { listVersion, idx1 ->
 
-    def jobName = "Test_${Common.projName} - Build ${Common.listVersion}}"
+    def jobName = "Test_${Common.projName} - Build ${Common.listVersion}"
     def desc = """
-    Job ${projName} is building with ${Common.listVersion}} version
+    Job ${projName} is building with ${Common.listVersion} version
     """
 
     pipelineJob(jobName) {
@@ -25,8 +27,10 @@ Common.list_projName.eachWithIndex { projName, idx ->
                 maxTotal(2)
                 categories(['cat-1'])
             }
+
+            environmentVariables(PROJ_NAME: '${Common.projName}', PROJ_VERSION: '${Common.listVersion}')
             
-            scm { //to be edited URl of project
+            scm { 
                 svn {
                     location('https://gsvnpapp1.germania-ins.com/svn/guidewire/applications/branches/Release_2.7.1/BillingCenter/modules/configuration@HEAD') {
                         directory('BillingCenter/Module/configuration')
@@ -34,8 +38,8 @@ Common.list_projName.eachWithIndex { projName, idx ->
                     }
                 }
                 svn {
-                    location('https://gsvnpapp1.germania-ins.com/svn/guidewire/applications/branches/Release_2.7.1/PolicyCenter/modules/configuration@HEAD') {
-                        directory('ext_temp/PolicyCenter/Module/configuration')
+                    location('https://gsvnpapp1.germania-ins.com/svn/guidewire/InsuranceSuite/applications/branches/Release_2.7.1/SuiteShared/modules/configuration@HEAD') {
+                        directory('ext_temp/BillingCenter/modules/configuration')
                         depth(SvnDepth.INFINITY)
                     }
                 }
